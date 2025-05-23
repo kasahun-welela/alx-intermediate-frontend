d425a3640888e07eb6c26a8b6a6d023bf4d30957
kasahun-welela <kasahunwelela1@gmail.com>
2025-05-23 23:56:30 +0300

    create teacher interface

---
+++ 0x04-TypeScript/js/main.ts
@@ -0,0 +1,17 @@
+interface Teacher {
+  firstName: string;
+  fullTimeEmployee: boolean;
+  lastName: string;
+  location: string;
+  contract: boolean;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3)
