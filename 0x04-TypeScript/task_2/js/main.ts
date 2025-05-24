cccf46d121f3fcde41eb8e44194f8370da7ab6b7
kasahun-welela <kasahunwelela1@gmail.com>
2025-05-24 16:01:11 +0300

    apply string literal

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -43,7 +43,40 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
   return new Director();
 }
-
 console.log(createEmployee(200)); 
 console.log(createEmployee(1000)); 
 console.log(createEmployee('$500')); 
+//6. Creating functions specific to employees
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+console.log(executeWork(createEmployee(200)));  // "Getting to work"
+console.log(executeWork(createEmployee(1000))); // Output: "Getting to director tasks"
+
+//7. String literal types
+
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+  
+}
+// Example usage
+console.log(teachClass('Math'));    //  "Teaching Math"
+console.log(teachClass('History')); // "Teaching History"
+
